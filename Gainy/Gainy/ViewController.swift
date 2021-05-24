a882b2b0295510f157edf53169b8dd65b2c08bb5
Dersim Davaod <dersim@gainy.app>
2021-05-24 20:26:19 +0300

    Add initial Gainy Xcode project (nested within Gainy workspace).

---
+++ Gainy/Gainy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gainy
+//
+//  Created by Dersim Davaod on 5/24/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
