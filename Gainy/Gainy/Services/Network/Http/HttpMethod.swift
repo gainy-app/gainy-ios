b2e383c66fa0016c5dff90b98ce6a3bfd9bf71a8
Dersim Davaod <dersim@gainy.app>
2021-05-25 16:39:21 +0300

    Add HTTP related structures.

---
+++ Gainy/Gainy/Services/Network/Http/HttpMethod.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+/**
+ HTTP method to make an HTTP request.
+ */
+enum HttpMethod: String {
+    /// HTTP GET method.
+    case get = "GET"
+}
---
+++ Gainy/Gainy/Services/Network/Http/HttpParameters.swift
@@ -0,0 +1,1 @@
+typealias HttpParameters = [String: Any]
