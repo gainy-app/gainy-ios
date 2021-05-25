3ae8988280c8eb9ad6e59bd1e13cc4ec70d79eef
Dersim Davaod <dersim@gainy.app>
2021-05-25 16:49:16 +0300

    Add invalidUrl case into the NetworkError.

--- Gainy/Gainy/Services/Network/NetworkError.swift
+++ Gainy/Gainy/Services/Network/NetworkError.swift
@@ -3,6 +3,9 @@ import Foundation
 /**
  Represents error occured while doing network request.
  */
-enum NetworkError: Error {}
+enum NetworkError: Error {
+    /// The URL in invalid.
+    case invalidUrl
+}
 
 extension NetworkError: LocalizedError {}
