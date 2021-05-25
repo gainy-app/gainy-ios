ecb52c45a8d615276a03e7ffd7039b5cdb021075
Dersim Davaod <dersim@gainy.app>
2021-05-25 20:37:29 +0300

    Rename initial ViewController to DiscoveryCollectionViewController.

--- Gainy/Gainy/Controllers/DiscoverCollectionsViewController.swift
+++ Gainy/Gainy/Controllers/DiscoverCollectionsViewController.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-class ViewController: UIViewController {
+class DiscoverCollectionsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
