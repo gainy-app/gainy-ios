fa42d506b4e03a75788ed2ac2ddf9836df31908d
Dersim Davaod <dersim@gainy.app>
2021-05-26 21:44:54 +0300

    Implement draft version of DiscoverCollectionsViewController.

--- Gainy/Gainy/Controllers/DiscoverCollectionsViewController.swift
+++ Gainy/Gainy/Controllers/DiscoverCollectionsViewController.swift
@@ -1,8 +1,309 @@
 import UIKit
 
+typealias CollectionsDataSource = UICollectionViewDiffableDataSource
+    <DiscoverCollectionsViewController.Section, Collection>
+typealias CollectionsDataSourceSnapshot = NSDiffableDataSourceSnapshot
+    <DiscoverCollectionsViewController.Section, Collection>
+
+// TODO: remove when testing is completed
+enum DummyDataSource {
+    static var collections = [
+        Collection(
+            id: 0,
+            name: "AI companies",
+            description: "Technology companies that utilize artificial intelligence",
+            stocksAmount: 15
+        ),
+        Collection(
+            id: 2,
+            name: "Global EVs",
+            description: "Global electric vehicles companies",
+            stocksAmount: 17
+        ),
+        Collection(
+            id: 3,
+            name: "Global Dividend",
+            description: "Global top rated dividended companies",
+            stocksAmount: 119
+        ),
+        Collection(
+            id: 4,
+            name: "Recent IPOs",
+            description: "Global IPOs that happen over last 6 months",
+            stocksAmount: 33
+        ),
+    ]
+
+    static var recommendedCollections = [
+        Collection(
+            id: 10,
+            name: "USA Fintech",
+            description: "Fintech companies of USA",
+            stocksAmount: 9
+        ),
+        Collection(
+            id: 12,
+            name: "Global EVs",
+            description: "Global electric vehicles companies",
+            stocksAmount: 17
+        ),
+        Collection(
+            id: 13,
+            name: "Small cap ETFs",
+            description: "ETFs of the stocks with market cap of $300M to $2B",
+            stocksAmount: 11
+        ),
+        Collection(
+            id: 14,
+            name: "Global Cannabis",
+            description: "Global cannabis companies stocks",
+            stocksAmount: 33
+        ),
+        Collection(
+            id: 15,
+            name: "Mid cap stocks",
+            description: "Companies with evaluation from $2B to $10B",
+            stocksAmount: 107
+        ),
+    ]
+}
+
 class DiscoverCollectionsViewController: UIViewController {
+    // MARK: - Internal
+
+    // MARK: Types
+
+    enum Section: Int, CaseIterable {
+        case discovered
+        case recommended
+
+        var columns: Int {
+            switch self {
+            case .discovered: return 1
+            case .recommended: return 3
+            }
+        }
+    }
+
+    // MARK: Lifecycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        setUpCollectionView()
+        updateSnapshot()
     }
+
+    // MARK: - Private
+
+    // MARK: IBOutlets
+
+    private var discoverCollectionsCollectionView: UICollectionView!
+
+    // MARK: Properties
+
+    private lazy var dataSource = configureDataSource()
+
+    // MARK: Functions
+
+    private func setUpCollectionView() {
+        // TODO: remove
+        for hashId in (400...900) {
+            DummyDataSource.recommendedCollections.append(
+                Collection(
+                    id: hashId,
+                    name: "AI companies",
+                    description: "Technology companies that utilize artificial intelligence",
+                    stocksAmount: 15
+                )
+            )
+        }
+
+        self.discoverCollectionsCollectionView = UICollectionView(frame: self.view.frame,
+                                                                  collectionViewLayout: appleLayout())
+        self.discoverCollectionsCollectionView.backgroundColor = UIColor(hexRgb: 0xE5E5E5)
+        self.discoverCollectionsCollectionView.showsVerticalScrollIndicator = false
+        self.discoverCollectionsCollectionView.dataSource = dataSource
+
+        self.view.addSubview(discoverCollectionsCollectionView)
+
+        self.discoverCollectionsCollectionView.register(
+            DiscoveredCollectionsHeaderView.self,
+            forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader,
+            withReuseIdentifier: DiscoveredCollectionsHeaderView.reuseIdentifier
+        )
+
+        self.discoverCollectionsCollectionView.register(
+            DiscoveredCollectionViewCell.self,
+            forCellWithReuseIdentifier: DiscoveredCollectionViewCell.reuseIdentifier
+        )
+
+        self.discoverCollectionsCollectionView.register(
+            RecommendedCollectionViewCell.self,
+            forCellWithReuseIdentifier: RecommendedCollectionViewCell.reuseIdentifier
+        )
+    }
+
+    private func configureDataSource() -> CollectionsDataSource {
+        let dataSource = CollectionsDataSource(
+            collectionView: discoverCollectionsCollectionView
+        ) { collectionView, indexPath, collection -> UICollectionViewCell? in
+            switch indexPath.section {
+            case Section.discovered.rawValue:
+                guard let cell = collectionView.dequeueReusableCell(
+                    withReuseIdentifier: DiscoveredCollectionViewCell.reuseIdentifier,
+                    for: indexPath
+                ) as? DiscoveredCollectionViewCell else {
+                    assertionFailure("FAILURE")
+                    return UICollectionViewCell()
+                }
+
+                cell.configureWith(name: collection.name,
+                                   description: collection.description,
+                                   stocksAmount: collection.stocksAmount)
+                return cell
+            case Section.recommended.rawValue:
+                guard let cell = collectionView.dequeueReusableCell(
+                    withReuseIdentifier: RecommendedCollectionViewCell.reuseIdentifier,
+                    for: indexPath
+                ) as? RecommendedCollectionViewCell else {
+                    assertionFailure("FAILURE")
+                    return UICollectionViewCell()
+                }
+
+                cell.configureWith(name: collection.name,
+                                   description: collection.description,
+                                   stocksAmount: collection.stocksAmount)
+                return cell
+            default:
+                return UICollectionViewCell()
+            }
+        }
+
+        dataSource.supplementaryViewProvider = { collectionView, kind, indexPath in
+            guard kind == UICollectionView.elementKindSectionHeader else {
+              return nil
+            }
+
+            let headerView = collectionView.dequeueReusableSupplementaryView(
+              ofKind: kind,
+              withReuseIdentifier: DiscoveredCollectionsHeaderView.reuseIdentifier,
+              for: indexPath) as? DiscoveredCollectionsHeaderView
+
+//            let section = self
+//                .dataSource
+//                .snapshot()
+//                .sectionIdentifiers[indexPath.section]
+//            headerView?.titleLabel.text = section.title
+            switch indexPath.section {
+            case Section.discovered.rawValue:
+                headerView?.configureWith(
+                    title: "Your collections",
+                    description: "Tap to view, swipe to edit or drag & drop to reorder"
+                )
+            case Section.recommended.rawValue:
+                headerView?.configureWith(
+                    title: "Collections you might like",
+                    description: "Tap on collection for preview, or tap on plus icon to add to your discovery"
+                )
+            default:
+                assertionFailure("FAILURE")
+            }
+            return headerView
+          }
+
+        return dataSource
+    }
+
+    private func updateSnapshot(animatingChange: Bool = false) {
+        var snapshot = CollectionsDataSourceSnapshot()
+        snapshot.appendSections(Section.allCases)
+        snapshot.appendItems(DummyDataSource.collections,
+                             toSection: .discovered)
+        snapshot.appendItems(DummyDataSource.recommendedCollections,
+                             toSection: .recommended)
+
+        dataSource.apply(snapshot, animatingDifferences: false)
+    }
+
+    private func appleLayout() -> UICollectionViewLayout {
+        UICollectionViewCompositionalLayout { (_ sectionIndex: Int, _: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection? in
+            guard let sectionKind = Section(rawValue: sectionIndex) else {
+                return nil
+            }
+
+            let columns = sectionKind.columns
+
+            // Items
+            let discoveryItemSize = NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1.0),
+                heightDimension: .fractionalHeight(1.0)
+            )
+            let discoveryItem = NSCollectionLayoutItem(layoutSize: discoveryItemSize)
+
+            let recommendedItemSize = NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1.0 / CGFloat(columns)),
+                heightDimension: .fractionalHeight(1.0)
+            )
+            let recommendedItem = NSCollectionLayoutItem(layoutSize: recommendedItemSize)
+            recommendedItem.contentInsets = NSDirectionalEdgeInsets(
+                top: 0.0,
+                leading: 4.0,
+                bottom: 0.0,
+                trailing: 4.0
+            )
+
+            // Group
+            let discoveredGroupSize = NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1.0),
+                heightDimension: .absolute(88)
+            )
+            let discoveredGroup = NSCollectionLayoutGroup.horizontal(
+                layoutSize: discoveredGroupSize,
+                subitems: [discoveryItem]
+            )
+            discoveredGroup.contentInsets = NSDirectionalEdgeInsets(
+                top: 0.0,
+                leading: 16.0,
+                bottom: 8.0,
+                trailing: 16.0
+            )
+
+            let recommendedGroupSize = NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1.0),
+                heightDimension: .absolute(144)
+            )
+            let recommendedGroup = NSCollectionLayoutGroup.horizontal(
+                layoutSize: recommendedGroupSize,
+                subitem: recommendedItem,
+                count: columns
+            )
+            recommendedGroup.contentInsets = NSDirectionalEdgeInsets(
+                top: 8.0,
+                leading: 14.0,
+                bottom: 0.0,
+                trailing: 14.0
+            )
+
+            let group = columns == 1 ? discoveredGroup : recommendedGroup
+            let section = NSCollectionLayoutSection(group: group)
+
+            let headerFooterSize = NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1.0),
+                heightDimension: .absolute(90)
+            )
+            let sectionHeader = NSCollectionLayoutBoundarySupplementaryItem(
+                layoutSize: headerFooterSize,
+                elementKind: UICollectionView.elementKindSectionHeader,
+                alignment: .top
+            )
+            section.boundarySupplementaryItems = [sectionHeader]
+
+            return section
+        }
+    }
+}
+
+// TODO: Use UICollectionViewDragDelegate/UICollectionViewDropDelegate
+extension DiscoverCollectionsViewController: UICollectionViewDelegate {
 }
