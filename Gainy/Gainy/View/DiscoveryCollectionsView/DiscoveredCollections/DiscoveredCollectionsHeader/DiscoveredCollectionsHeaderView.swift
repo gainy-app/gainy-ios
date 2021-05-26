ba7ff90e9a771d45284efdf5b61e7ae28002f4aa
Dersim Davaod <dersim@gainy.app>
2021-05-27 00:43:38 +0300

    Update DiscoveredCollectionsHeaderView: clean up code and avoid blending for layers.

--- Gainy/Gainy/View/DiscoveryCollectionsView/DiscoveredCollections/DiscoveredCollectionsHeader/DiscoveredCollectionsHeaderView.swift
+++ Gainy/Gainy/View/DiscoveryCollectionsView/DiscoveredCollections/DiscoveredCollectionsHeader/DiscoveredCollectionsHeaderView.swift
@@ -7,6 +7,34 @@ class DiscoveredCollectionsHeaderView: UICollectionReusableView {
         super.init(frame: frame)
 
         self.backgroundColor = UIColor.Gainy.white
+
+        self.addSubview(titleLabel)
+        self.addSubview(descriptionLabel)
+
+        NSLayoutConstraint.activate([
+            titleLabel
+                .leadingAnchor
+                .constraint(equalTo: self.leadingAnchor, constant: 28),
+            titleLabel
+                .trailingAnchor
+                .constraint(equalTo: self.trailingAnchor, constant: -28),
+            titleLabel
+                .topAnchor
+                .constraint(equalTo: topAnchor, constant: 12),
+            titleLabel
+                .bottomAnchor
+                .constraint(equalTo: descriptionLabel.topAnchor, constant: -4),
+
+            descriptionLabel
+                .leadingAnchor
+                .constraint(equalTo: self.leadingAnchor, constant: 28),
+            descriptionLabel
+                .trailingAnchor
+                .constraint(equalTo: self.trailingAnchor, constant: -28),
+            descriptionLabel
+                .bottomAnchor
+                .constraint(lessThanOrEqualTo: self.bottomAnchor, constant: 28),
+        ])
     }
 
     @available(*, unavailable)
@@ -26,19 +54,14 @@ class DiscoveredCollectionsHeaderView: UICollectionReusableView {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
 
-        // label.font = UIFont(name: "SFProDisplay-Semibold", size: 20)
-        label.font = UIFont.systemFont(
-            ofSize: 20,
-            weight: .semibold
-        )
+        // TODO: UIFont(name: "SFProDisplay-Semibold", size: 20)
+        label.font = UIFont.systemFont(ofSize: 20, weight: .semibold)
         label.textColor = UIColor.Gainy.textDark
-        label.numberOfLines = 0
-        label.lineBreakMode = .byWordWrapping
+        label.backgroundColor = UIColor.Gainy.white
+
+        label.numberOfLines = 1
+        label.lineBreakMode = .byTruncatingTail
         label.textAlignment = .left
-        label.setContentCompressionResistancePriority(
-            .defaultHigh,
-            for: .horizontal
-        )
 
         return label
     }()
@@ -47,19 +70,14 @@ class DiscoveredCollectionsHeaderView: UICollectionReusableView {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
 
-        // label.font = UIFont(name: "SFProDisplay-Regular", size: 14)
-        label.font = UIFont.systemFont(
-            ofSize: 14,
-            weight: .regular
-        )
+        // TODO: UIFont(name: "SFProDisplay-Regular", size: 14)
+        label.font = UIFont.systemFont(ofSize: 14, weight: .regular)
         label.textColor = UIColor.Gainy.darkGray
+        label.backgroundColor = UIColor.Gainy.white
+
         label.numberOfLines = 0
         label.lineBreakMode = .byWordWrapping
         label.textAlignment = .left
-        label.setContentCompressionResistancePriority(
-            .defaultHigh,
-            for: .horizontal
-        )
 
         return label
     }()
@@ -67,47 +85,11 @@ class DiscoveredCollectionsHeaderView: UICollectionReusableView {
     // MARK: Functions
 
     func configureWith(title: String, description: String) {
-        self.title = title
         titleLabel.text = title
         descriptionLabel.text = description
-        self.addSubview(titleLabel)
-        self.addSubview(descriptionLabel)
-
-        NSLayoutConstraint.activate([
-            titleLabel.leadingAnchor.constraint(
-                equalTo: self.leadingAnchor,
-                constant: 28
-            ),
-
-            titleLabel.trailingAnchor.constraint(
-                lessThanOrEqualTo: self.trailingAnchor,
-                constant: -28
-            ),
-            titleLabel.topAnchor.constraint(equalTo: topAnchor, constant: 24),
-            titleLabel.bottomAnchor.constraint(equalTo: descriptionLabel.topAnchor, constant: 0),
-        ])
-
-        NSLayoutConstraint.activate([
-            descriptionLabel.leadingAnchor.constraint(
-                equalTo: self.leadingAnchor,
-                constant: 28
-            ),
-
-            descriptionLabel.trailingAnchor.constraint(
-                lessThanOrEqualTo: self.trailingAnchor,
-                constant: -28
-            ),
-
-            descriptionLabel.bottomAnchor.constraint(
-                lessThanOrEqualTo: self.bottomAnchor,
-                constant: 28
-            ),
-        ])
     }
 
     // MARK: Private
 
     // MARK: Properties
-
-    private var title: String = ""
 }
