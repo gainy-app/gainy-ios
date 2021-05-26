89dc68c5677696e21e8162a6900a8617ad219e3d
Dersim Davaod <dersim@gainy.app>
2021-05-26 22:44:22 +0300

    Add more stubs for colors into the UIColor+Gainy extension.

--- Gainy/Gainy/Extensions/UIColor+Gainy.swift
+++ Gainy/Gainy/Extensions/UIColor+Gainy.swift
@@ -2,19 +2,34 @@ import UIKit
 
 extension UIColor {
     enum Gainy {
-        static let textDark = UIColor(red: 31.0 / 255.0,
-                                      green: 46.0 / 255.0,
-                                      blue: 53.0 / 255.0,
-                                      alpha: 1)
+//        static let red
+        static let yellow = UIColor(red: 255.0 / 255.0,
+                                    green: 245.0 / 255.0,
+                                    blue: 0.0 / 255.0,
+                                    alpha: 1)
+
+//        static let green
+//        static let lightBlue
+//        static let blue
+//        static let white
+//        static let lightBack
+//        static let back
+//        static let lightGray
+//        static let gray
 
         static let darkGray = UIColor(red: 104.0 / 255.0,
                                       green: 115.0 / 255.0,
                                       blue: 121.0 / 255.0,
                                       alpha: 1)
 
-        static let yellow = UIColor(red: 255.0 / 255.0,
-                                    green: 245.0 / 255.0,
-                                    blue: 0.0 / 255.0,
-                                    alpha: 1)
+//        static let grayNotDark
+
+        static let textDark = UIColor(red: 31.0 / 255.0,
+                                      green: 46.0 / 255.0,
+                                      blue: 53.0 / 255.0,
+                                      alpha: 1)
+
+//        static let black
+//        static let purple
     }
 }
