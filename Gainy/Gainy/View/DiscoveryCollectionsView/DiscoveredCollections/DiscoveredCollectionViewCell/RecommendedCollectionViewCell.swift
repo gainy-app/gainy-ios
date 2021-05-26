a9e2f022b6ffcf9c70eac71c254b883a04717763
Dersim Davaod <dersim@gainy.app>
2021-05-27 01:08:13 +0300

    Update RecommendedCollectionViewCell.

--- Gainy/Gainy/View/DiscoveryCollectionsView/DiscoveredCollections/DiscoveredCollectionViewCell/RecommendedCollectionViewCell.swift
+++ Gainy/Gainy/View/DiscoveryCollectionsView/DiscoveredCollections/DiscoveredCollectionViewCell/RecommendedCollectionViewCell.swift
@@ -6,8 +6,88 @@ class RecommendedCollectionViewCell: UICollectionViewCell {
     override init(frame _: CGRect) {
         super.init(frame: .zero)
 
-        self.layer.cornerRadius = 8
-        self.backgroundColor = .orange
+        self.backgroundColor = UIColor.Gainy.white
+
+        self.addSubview(nameLabel)
+        self.addSubview(descriptionLabel)
+        self.addSubview(stocksLabel)
+        self.addSubview(stocksAmountLabel)
+        self.addSubview(plusButton)
+        plusButton.setImage(UIImage(named: "plus"), for: .normal)
+
+        NSLayoutConstraint.activate([
+            nameLabel
+                .leadingAnchor
+                .constraint(equalTo: self.leadingAnchor, constant: 8),
+            nameLabel
+                .trailingAnchor
+                .constraint(equalTo: self.trailingAnchor, constant: -8),
+            nameLabel
+                .topAnchor
+                .constraint(equalTo: topAnchor, constant: 12),
+            nameLabel
+                .bottomAnchor
+                .constraint(equalTo: descriptionLabel.topAnchor, constant: -4),
+
+            descriptionLabel
+                .leadingAnchor
+                .constraint(equalTo: self.leadingAnchor, constant: 8),
+            descriptionLabel
+                .trailingAnchor
+                .constraint(equalTo: self.trailingAnchor, constant: -8),
+            descriptionLabel
+                .heightAnchor
+                .constraint(lessThanOrEqualToConstant: 40),
+            descriptionLabel
+                .bottomAnchor
+                .constraint(greaterThanOrEqualTo: stocksLabel.topAnchor, constant: -7),
+
+            stocksLabel
+                .leadingAnchor
+                .constraint(equalTo: self.leadingAnchor, constant: 8),
+            stocksLabel
+                .heightAnchor
+                .constraint(equalToConstant: 10),
+            stocksLabel
+                .widthAnchor
+                .constraint(equalToConstant: 50),
+            stocksLabel
+                .bottomAnchor
+                .constraint(equalTo: stocksAmountLabel.topAnchor, constant: 0),
+
+            stocksAmountLabel
+                .leadingAnchor
+                .constraint(equalTo: self.leadingAnchor, constant: 8),
+            stocksAmountLabel
+                .bottomAnchor
+                .constraint(equalTo: self.bottomAnchor, constant: -12),
+            stocksAmountLabel
+                .heightAnchor
+                .constraint(equalToConstant: 24),
+            stocksAmountLabel
+                .widthAnchor
+                .constraint(equalToConstant: 50),
+
+            plusButton
+                .trailingAnchor
+                .constraint(equalTo: self.trailingAnchor, constant: -8),
+            plusButton
+                .bottomAnchor
+                .constraint(equalTo: self.bottomAnchor, constant: -8),
+            plusButton
+                .widthAnchor
+                .constraint(equalToConstant: 28),
+            plusButton
+                .heightAnchor
+                .constraint(equalToConstant: 28),
+        ])
+    }
+
+    override func draw(_ rect: CGRect) {
+        let borderPath = UIBezierPath(roundedRect: self.bounds,
+                                      cornerRadius: cornerRadius)
+        UIColor.orange.set() // TODO: update with a picture
+        borderPath.fill()
     }
 
     @available(*, unavailable)
@@ -29,19 +109,13 @@ class RecommendedCollectionViewCell: UICollectionViewCell {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
 
-//        UIFont(name: "SFProDisplay-Bold", size: 16)
-        label.font = UIFont.systemFont(
-            ofSize: 16,
-            weight: .bold
-        )
-        label.textColor = .white
+        // TODO: UIFont(name: "SFProDisplay-Bold", size: 16)
+        label.font = UIFont.systemFont(ofSize: 16, weight: .bold)
+        label.textColor = UIColor.Gainy.white
+
         label.numberOfLines = 0
         label.lineBreakMode = .byWordWrapping
         label.textAlignment = .left
-        label.setContentCompressionResistancePriority(
-            .defaultHigh,
-            for: .horizontal
-        )
 
         return label
     }()
@@ -50,19 +124,13 @@ class RecommendedCollectionViewCell: UICollectionViewCell {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
 
-//        UIFont(name: "SFProDisplay-Regular", size: 12)
-        label.font = UIFont.systemFont(
-            ofSize: 12,
-            weight: .regular
-        )
-        label.textColor = .white
+        // TODO: UIFont(name: "SFProDisplay-Regular", size: 12)
+        label.font = UIFont.systemFont(ofSize: 12, weight: .regular)
+        label.textColor = UIColor.Gainy.white
+
         label.numberOfLines = 0
         label.lineBreakMode = .byWordWrapping
         label.textAlignment = .left
-        label.setContentCompressionResistancePriority(
-            .defaultHigh,
-            for: .horizontal
-        )
 
         return label
     }()
@@ -71,18 +139,12 @@ class RecommendedCollectionViewCell: UICollectionViewCell {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
 
-//        UIFont(name: "SFCompactRounded-Medium", size: 9)
-        label.font = UIFont.systemFont(
-            ofSize: 9,
-            weight: .medium
-        )
-        label.textColor = .white
+        // TODO: UIFont(name: "SFCompactRounded-Medium", size: 9)
+        label.font = UIFont.systemFont(ofSize: 9, weight: .medium)
+        label.textColor = UIColor.Gainy.white
+
         label.numberOfLines = 1
         label.textAlignment = .left
-        label.setContentCompressionResistancePriority(
-            .defaultHigh,
-            for: .horizontal
-        )
 
         return label
     }()
@@ -90,130 +152,70 @@ class RecommendedCollectionViewCell: UICollectionViewCell {
     lazy var stocksAmountLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
-//       UIFont(name: "SFCompactRounded-Semibold", size: 28)
-        label.font = UIFont.systemFont(
-            ofSize: 28,
-            weight: .semibold
-        )
+
+        // TODO: UIFont(name: "SFCompactRounded-Semibold", size: 28)
+        label.font = UIFont.systemFont(ofSize: 28, weight: .semibold)
         label.textColor = UIColor.Gainy.yellow
+
         label.numberOfLines = 1
         label.textAlignment = .left
-        label.setContentCompressionResistancePriority(
-            .defaultHigh,
-            for: .horizontal
-        )
 
         return label
     }()
 
     lazy var plusButton: UIButton = {
-        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 28, height: 28))
+        let button = UIButton()
         button.translatesAutoresizingMaskIntoConstraints = false
-        button.layer.cornerRadius = 14
-        button.backgroundColor = .white
 
-        // TODO: make a robust centering
-        let imageView = UIImageView(frame: CGRect(x: 8.33, y: 8.33, width: 11.33, height: 11.33))
+        button.layer.cornerRadius = 14
+        button.backgroundColor = UIColor.Gainy.white
+
+        button.addTarget(self,
+                         action: #selector(self.plusButtonTapped(_:)),
+                         for: .touchUpInside)
+
+        var shadows = UIView()
+        shadows.frame = button.frame
+        shadows.clipsToBounds = false
+        button.addSubview(shadows)
+
+        let shadowPath0 = UIBezierPath(roundedRect: shadows.bounds, cornerRadius: 0)
+        let layer0 = CALayer()
+        layer0.shadowPath = shadowPath0.cgPath
+        layer0.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.1).cgColor
+        layer0.shadowOpacity = 1
+        layer0.shadowRadius = 6
+        layer0.shadowOffset = CGSize(width: 0, height: 2)
+        layer0.bounds = shadows.bounds
+        layer0.position = shadows.center
+        shadows.layer.addSublayer(layer0)
+
+        return button
+    }()
+
+    lazy var plusImageView: UIImageView = {
+        let imageView = UIImageView()
+
         imageView.image = UIImage(named: "plus")
         imageView.contentMode = .center
 
-        button.addTarget(self, action: #selector(self.plusButtonTapped(_:)), for: .touchUpInside)
-
-        button.addSubview(imageView)
-
-        return button
+        return imageView
     }()
 
     // MARK: Functions
 
     func configureWith(name: String, description: String, stocksAmount: Int) {
-        self.name = name
-        self.desc = description
-        self.stocksAmount = stocksAmount
-
         nameLabel.text = name
         descriptionLabel.text = description
         stocksLabel.text = "STOCKS"
         stocksAmountLabel.text = "\(stocksAmount)"
-
-        self.addSubview(nameLabel)
-        self.addSubview(descriptionLabel)
-        self.addSubview(stocksLabel)
-        self.addSubview(stocksAmountLabel)
-        self.addSubview(plusButton)
-
-        NSLayoutConstraint.activate([
-            nameLabel.leadingAnchor.constraint(
-                equalTo: self.leadingAnchor,
-                constant: 8
-            ),
-
-            nameLabel.trailingAnchor.constraint(
-                equalTo: self.trailingAnchor,
-                constant: -8
-            ),
-
-            nameLabel.topAnchor.constraint(equalTo: topAnchor, constant: 8),
-            nameLabel.bottomAnchor.constraint(equalTo: descriptionLabel.topAnchor, constant: 8),
-        ])
-
-        NSLayoutConstraint.activate([
-            descriptionLabel.leadingAnchor.constraint(
-                equalTo: self.leadingAnchor,
-                constant: 8
-            ),
-
-            descriptionLabel.trailingAnchor.constraint(
-                equalTo: self.trailingAnchor,
-                constant: -8
-            ),
-
-            descriptionLabel.bottomAnchor.constraint(greaterThanOrEqualTo: stocksLabel.topAnchor, constant: 7),
-        ])
-
-        NSLayoutConstraint.activate([
-            stocksLabel.leadingAnchor.constraint(
-                equalTo: self.leadingAnchor,
-                constant: 8
-            ),
-
-            stocksLabel.widthAnchor.constraint(equalToConstant: 50),
-
-            stocksLabel.bottomAnchor.constraint(equalTo: stocksAmountLabel.topAnchor, constant: 0),
-        ])
-
-        NSLayoutConstraint.activate([
-            stocksAmountLabel.leadingAnchor.constraint(
-                equalTo: self.leadingAnchor,
-                constant: 8
-            ),
-
-            stocksAmountLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -12),
-        ])
-
-        NSLayoutConstraint.activate([
-            plusButton.trailingAnchor.constraint(
-                equalTo: self.trailingAnchor,
-                constant: -8
-            ),
-
-            plusButton.bottomAnchor.constraint(
-                equalTo: self.bottomAnchor,
-                constant: -12
-            ),
-
-            plusButton.widthAnchor.constraint(equalToConstant: 28),
-            plusButton.heightAnchor.constraint(equalToConstant: 28),
-        ])
     }
 
     // MARK: Private
 
     // MARK: Properties
 
-    private var name: String = ""
-    private var desc: String = ""
-    private var stocksAmount: Int = 0
+    private let cornerRadius: CGFloat = 8
 
     // MARK: Functions
 
