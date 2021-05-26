fc36eb4b2f0682da08a7979b2af059a4ad486a11
Dersim Davaod <dersim@gainy.app>
2021-05-26 20:37:15 +0300

    Add draft version of UIColor's extension to use in the app.

--- Gainy/Gainy/View/DiscoveryCollectionsView/DiscoveredCollections/DiscoveredCollectionsHeader/DiscoveredCollectionsHeaderView.swift
+++ Gainy/Gainy/View/DiscoveryCollectionsView/DiscoveredCollections/DiscoveredCollectionsHeader/DiscoveredCollectionsHeaderView.swift
@@ -1,5 +1,27 @@
 import UIKit
 
+// TODO: move into the separate class
+extension UIColor {
+    convenience init(red: Int, green: Int, blue: Int) {
+        assert((0...255).contains(red), "Invalid red component")
+        assert((0...255).contains(green), "Invalid green component")
+        assert((0...255).contains(blue), "Invalid blue component")
+
+       self.init(red: CGFloat(red) / 255.0,
+                 green: CGFloat(green) / 255.0,
+                 blue: CGFloat(blue) / 255.0,
+                 alpha: 1.0)
+   }
+
+   convenience init(hexRgb: Int) {
+       self.init(
+           red: (hexRgb >> 16) & 0xFF,
+           green: (hexRgb >> 8) & 0xFF,
+           blue: hexRgb & 0xFF
+       )
+   }
+}
+
 class DiscoveredCollectionsHeaderView: UICollectionReusableView {
     // MARK: - Internal
 
