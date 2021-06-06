6a1bc0ee788b91cbe17e3803515a3adac4173218
Dersim Davaod <dersim@gainy.app>
2021-06-06 21:12:37 +0300

    Add more definitions for colours in the UIColor+Gainy extension.

--- Gainy/Gainy/Extensions/UIColor+Gainy.swift
+++ Gainy/Gainy/Extensions/UIColor+Gainy.swift
@@ -3,7 +3,9 @@ import UIKit
 extension UIColor {
     enum Gainy {
         static let white = UIColor.white
+
 //        static let red
+
         static let yellow = UIColor(red: 255.0 / 255.0,
                                     green: 245.0 / 255.0,
                                     blue: 0.0 / 255.0,
@@ -13,7 +15,11 @@ extension UIColor {
 //        static let lightBlue
 //        static let blue
 //        static let white
-//        static let lightBack
+
+        static let lightBack = UIColor(red: 247.0 / 255.0,
+                                       green: 248.0 / 255.0,
+                                       blue: 249.0 / 255.0,
+                                       alpha: 1)
 
         static let back = UIColor(red: 238.0 / 255.0,
                                   green: 243.0 / 255.0,
@@ -21,7 +27,11 @@ extension UIColor {
                                   alpha: 1)
 
 //        static let lightGray
-//        static let gray
+
+        static let gray = UIColor(red: 135.0 / 255.0,
+                                  green: 144.0 / 255.0,
+                                  blue: 149.0 / 255.0,
+                                  alpha: 1)
 
         static let darkGray = UIColor(red: 104.0 / 255.0,
                                       green: 115.0 / 255.0,
