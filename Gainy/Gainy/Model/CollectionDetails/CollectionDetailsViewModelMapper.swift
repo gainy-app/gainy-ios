89db15d6e7ae31809d16b8d340f5a817579cab21
Dersim Davaod <dersim@gainy.app>
2021-06-10 22:57:25 +0300

    Extract extensions for Float type into the Float+CleanDecimals.swift.

--- Gainy/Gainy/Model/CollectionDetails/CollectionDetailsViewModelMapper.swift
+++ Gainy/Gainy/Model/CollectionDetails/CollectionDetailsViewModelMapper.swift
@@ -1,18 +1,3 @@
-// TODO: 1: move to separate file
-extension Float {
-    var cleanOneDecimal: String {
-        self.truncatingRemainder(dividingBy: 1) == 0
-            ? String(format: "%.0f", self)
-            : String(format: "%.1f", self)
-    }
-
-    var cleanTwoDecimal: String {
-        self.truncatingRemainder(dividingBy: 1) == 0
-            ? String(format: "%.0f", self)
-            : String(format: "%.2f", self)
-    }
-}
-
 enum CollectionDetailsViewModelMapper {
     static func map(_ model: CollectionDetails) -> CollectionDetailViewCellModel {
         CollectionDetailViewCellModel(
