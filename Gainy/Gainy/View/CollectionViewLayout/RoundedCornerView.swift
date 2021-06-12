1d5ea4b545d402078c572c0b9d8916ad278cab56
Dersim Davaod <dersim@gainy.app>
2021-06-12 14:59:28 +0300

    Update an approach to draw rounded corners in CollectionView's cells.

--- Gainy/Gainy/View/CollectionViewLayout/RoundedCornerView.swift
+++ Gainy/Gainy/View/CollectionViewLayout/RoundedCornerView.swift
@@ -4,16 +4,46 @@ class RoundedCornerView: UICollectionViewCell {
     // MARK: Internal
 
     override func draw(_: CGRect) {
-        let borderPath = UIBezierPath(
-            roundedRect: bounds,
-            cornerRadius: Constant.cornerRadius
-        )
+        let borderPath = maskBezierPath()
         borderPath.fill()
+
+        let mask = CAShapeLayer()
+        mask.path = borderPath.cgPath
+        self.layer.mask = mask
     }
 
     // MARK: Private
 
+    // MARK: Types
+
     private enum Constant {
         static let cornerRadius = 8.0 as CGFloat
+    }
+
+    // MARK: Functions
+
+    private func maskBezierPath() -> UIBezierPath {
+        let path = UIBezierPath()
+
+        path.append(
+            UIBezierPath(
+                roundedRect: bounds,
+                cornerRadius: Constant.cornerRadius
+            )
+        )
+
+        path.append(
+            UIBezierPath(
+                roundedRect: CGRect(
+                    x: bounds.width + 16,
+                    y: 20,
+                    width: 48,
+                    height: 48
+                ),
+                cornerRadius: Constant.cornerRadius
+            )
+        )
+
+        return path
     }
 }
