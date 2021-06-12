b3723c4f285fb14d458b5be9891246ad0a3180f3
Dersim Davaod <dersim@gainy.app>
2021-06-12 12:24:48 +0300

    Change the height of the RecommendedCollectionViewCell from 155 to 144.

--- Gainy/Gainy/View/DiscoveryCollectionsView/DiscoveredCollections/CollectionSection/RecommendedCollectionsSectionLayout.swift
+++ Gainy/Gainy/View/DiscoveryCollectionsView/DiscoveredCollections/CollectionSection/RecommendedCollectionsSectionLayout.swift
@@ -16,7 +16,7 @@ struct RecommendedCollectionsSectionLayout: SectionLayout {
         let recommendedGroup = NSCollectionLayoutGroup.horizontal(
             layoutSize: NSCollectionLayoutSize(
                 widthDimension: .fractionalWidth(1.0),
-                heightDimension: .absolute(155) // original is 144
+                heightDimension: .absolute(144)
             ),
             subitem: recommendedItem,
             count: Constant.numberOfColumns
