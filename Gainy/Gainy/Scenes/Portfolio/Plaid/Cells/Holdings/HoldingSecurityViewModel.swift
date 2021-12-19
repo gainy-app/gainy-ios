b5b54f9efa7b84e94f8a5f1fc0acf4913fa1b8e8
Anton Gubarenko <anton@gainy.app>
2021-12-19 21:54:06 +0300

    RC improvements

--- Gainy/Gainy/Scenes/Portfolio/Plaid/Cells/Holdings/HoldingSecurityViewModel.swift
+++ Gainy/Gainy/Scenes/Portfolio/Plaid/Cells/Holdings/HoldingSecurityViewModel.swift
@@ -20,8 +20,11 @@ struct HoldingSecurityViewModel {
     let absoluteGains: [ScatterChartView.ChartPeriod : Float]
     let relativeGains: [ScatterChartView.ChartPeriod : Float]
     
-    func infoForRange(_ range: ScatterChartView.ChartPeriod) -> (String, UIImage, String, String) {
-        return (range.longName, UIImage(named: relativeGains[range] ?? 0.0 >= 0.0 ?  "small_up" : "small_down")!, absoluteGains[range]?.price ?? "", relativeGains[range]?.cleanOneDecimalP ?? "" )
+    func infoForRange(_ range: ScatterChartView.ChartPeriod) -> (String, UIImage, String, String, UIColor?, UIColor?) {
+        return (range.longName, UIImage(named: relativeGains[range] ?? 0.0 >= 0.0 ?  "small_up" : "small_down")!, absoluteGains[range]?.price ?? "",
+                relativeGains[range]?.cleanOneDecimalP ?? "",
+                relativeGains[range] ?? 0.0 >= 0.0 ? UIColor(named: "mainGreen") :  UIColor(named: "mainRed"),
+                relativeGains[range] ?? 0.0 >= 0.0 ? UIColor(named: "mainGreen") :  UIColor(named: "mainRed"))
         
     }
     
