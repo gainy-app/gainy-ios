7eeb49e7314b93383e9ad8712f26549681bd5514
Anton Gubarenko <anton@gainy.com>
2022-01-31 16:59:10 +0300

    App version added

---
+++ Gainy/Gainy/Extensions/Bundle+Version.swift
@@ -0,0 +1,20 @@
+//
+//  Bundle+Version.swift
+//  Gainy
+//
+//  Created by Anton Gubarenko on 31.01.2022.
+//
+
+import Foundation
+
+extension Bundle {
+    var releaseVersionNumber: String? {
+        return infoDictionary?["CFBundleShortVersionString"] as? String
+    }
+    var buildVersionNumber: String? {
+        return infoDictionary?["CFBundleVersion"] as? String
+    }
+    var releaseVersionNumberPretty: String {
+        return "v\(releaseVersionNumber ?? "1.0.0")"
+    }
+}
--- Gainy/Gainy/Scenes/Profile/ProfileViewController.swift
+++ Gainy/Gainy/Scenes/Profile/ProfileViewController.swift
@@ -41,6 +41,11 @@ final class ProfileViewController: BaseViewController {
     @IBOutlet private weak var interestsCollectionView: UICollectionView!
     @IBOutlet private weak var profilePictureImageView: UIImageView!
     @IBOutlet private weak var fullNameTitle: UILabel!
+    @IBOutlet weak var versionLbl: UILabel! {
+        didSet {
+            versionLbl.text = "\(Bundle.main.releaseVersionNumberPretty) #\(Bundle.main.buildVersionNumber)"
+        }
+    }
     
     private var currentCollectionView: UICollectionView?
     private var currentIndexPath: IndexPath?
