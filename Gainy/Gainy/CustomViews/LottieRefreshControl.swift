38c7ea53b744a127b11621f819d136369227a752
Serhii Borysov <serhii@beat.no>
2022-02-02 13:23:59 +0200

    Fixed using refresh control in background thread crash

--- Gainy/Gainy/CustomViews/LottieRefreshControl.swift
+++ Gainy/Gainy/CustomViews/LottieRefreshControl.swift
@@ -8,59 +8,69 @@
 import UIKit
 import Lottie
 
-class LottieRefreshControl: UIRefreshControl {
-fileprivate let animationView = Lottie.AnimationView(animation: Animation.named("loader3"))
-fileprivate var isAnimating = false
-
+final class LottieRefreshControl: UIRefreshControl {
+    fileprivate let animationView = Lottie.AnimationView(animation: Animation.named("loader3"))
+    fileprivate var isAnimating = false
+    
     fileprivate let maxPullDistance: CGFloat = 32.0 * 3.0
-
-override init() {
-    super.init(frame: .zero)
-    setupView()
-    setupLayout()
-}
-
-required init?(coder aDecoder: NSCoder) {
-    fatalError("init(coder:) has not been implemented")
-}
-
-func updateProgress(with offsetY: CGFloat) {
-    guard !isAnimating else { return }
-    let progress = min(abs(offsetY / maxPullDistance), 1)
-    animationView.currentProgress = progress
-}
-
-override func beginRefreshing() {
-    super.beginRefreshing()
-    isAnimating = true
-    animationView.currentProgress = 0
-    animationView.play()
-}
-
-override func endRefreshing() {
-    super.endRefreshing()
-    animationView.stop()
-    isAnimating = false
-}
+    
+    override init() {
+        super.init(frame: .zero)
+        setupView()
+        setupLayout()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func updateProgress(with offsetY: CGFloat) {
+        runOnMain {
+            guard !isAnimating else { return }
+            let progress = min(abs(offsetY / maxPullDistance), 1)
+            animationView.currentProgress = progress
+        }
+    }
+    
+    override func beginRefreshing() {
+        runOnMain {
+            super.beginRefreshing()
+            isAnimating = true
+            animationView.currentProgress = 0
+            animationView.play()
+        }
+    }
+    
+    override func endRefreshing() {
+        runOnMain {
+            super.endRefreshing()
+            animationView.stop()
+            isAnimating = false
+        }
+    }
 }
 
 private extension LottieRefreshControl {
-func setupView() {
-    // hide default indicator view
-    tintColor = .clear
-    animationView.loopMode = .loop
-    addSubview(animationView)
-
-    addTarget(self, action: #selector(beginRefreshing), for: .valueChanged)
-}
-
-func setupLayout() {
-    animationView.translatesAutoresizingMaskIntoConstraints = false
-    NSLayoutConstraint.activate([
-        animationView.centerXAnchor.constraint(equalTo: centerXAnchor),
-        animationView.centerYAnchor.constraint(equalTo: centerYAnchor),
-        animationView.widthAnchor.constraint(equalToConstant: 32),
-        animationView.heightAnchor.constraint(equalToConstant: 32)
-    ])
-}
+    func setupView() {
+        runOnMain {
+            // hide default indicator view
+            tintColor = .clear
+            animationView.loopMode = .loop
+            addSubview(animationView)
+            
+            addTarget(self, action: #selector(beginRefreshing), for: .valueChanged)
+        }
+    }
+    
+    func setupLayout() {
+        runOnMain {
+            animationView.translatesAutoresizingMaskIntoConstraints = false
+            NSLayoutConstraint.activate([
+                animationView.centerXAnchor.constraint(equalTo: centerXAnchor),
+                animationView.centerYAnchor.constraint(equalTo: centerYAnchor),
+                animationView.widthAnchor.constraint(equalToConstant: 32),
+                animationView.heightAnchor.constraint(equalToConstant: 32)
+            ])
+        }
+    }
 }
