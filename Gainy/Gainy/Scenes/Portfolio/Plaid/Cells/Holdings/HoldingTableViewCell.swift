31f8d619d17c3c8b555ec534834f0a548c27ac0d
Anton Gubarenko <anton@gainy.com>
2022-03-31 16:22:49 +0300

    Crypto cell updated

--- Gainy/Gainy/Scenes/Portfolio/Plaid/Cells/Holdings/HoldingTableViewCell.swift
+++ Gainy/Gainy/Scenes/Portfolio/Plaid/Cells/Holdings/HoldingTableViewCell.swift
@@ -284,7 +284,7 @@ extension HoldingTableViewCell: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell: HoldingSecurityTableViewCell = tableView.dequeueReusableCell(withIdentifier: (holding?.isCash ?? false) ? HoldingSecurityTableViewCell.cashCellIdentifier : HoldingSecurityTableViewCell.cellIdentifier, for: indexPath) as! HoldingSecurityTableViewCell
+        let cell: HoldingSecurityTableViewCell = tableView.dequeueReusableCell(withIdentifier: ((holding?.isCash ?? false) || (holding?.isCrypro ?? false)) ? HoldingSecurityTableViewCell.cashCellIdentifier : HoldingSecurityTableViewCell.cellIdentifier, for: indexPath) as! HoldingSecurityTableViewCell
         if let security = holding?.securities[indexPath.row] {
             cell.setModel(security, chartRange)
         }
