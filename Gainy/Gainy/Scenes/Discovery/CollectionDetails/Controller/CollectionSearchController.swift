7293a67788addcc9d7b66e6631045060f0138abb
Anton Gubarenko <anton@gainy.com>
2022-04-14 15:09:13 +0300

    MS Collections update

--- Gainy/Gainy/Scenes/Discovery/CollectionDetails/Controller/CollectionSearchController.swift
+++ Gainy/Gainy/Scenes/Discovery/CollectionDetails/Controller/CollectionSearchController.swift
@@ -482,7 +482,7 @@ extension CollectionSearchController {
                 item.contentInsets = NSDirectionalEdgeInsets(top: 4, leading: 4, bottom: 4, trailing: 4)
                 
                 let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
-                                                       heightDimension: NSCollectionLayoutDimension.absolute(144))
+                                                       heightDimension: NSCollectionLayoutDimension.absolute(152))
                 let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: 3)
                 
                 let section = NSCollectionLayoutSection(group: group)
