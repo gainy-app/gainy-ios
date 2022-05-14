cd5ec0be213b70461b5920f8d8ba59f6f28beecd
Anton Gubarenko <anton@gainy.app>
2022-05-14 19:48:09 +0300

    Framework added

---
+++ Gainy/CommonLibs/CommonLibs/CommonLibs.h
@@ -0,0 +1,18 @@
+//
+//  CommonLibs.h
+//  CommonLibs
+//
+//  Created by Anton Gubarenko on 14.05.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CommonLibs.
+FOUNDATION_EXPORT double CommonLibsVersionNumber;
+
+//! Project version string for CommonLibs.
+FOUNDATION_EXPORT const unsigned char CommonLibsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CommonLibs/PublicHeader.h>
+
+
---
+++ Gainy/CommonLibs/CommonLibs/SharedLogic.swift
@@ -0,0 +1,8 @@
+//
+//  SharedLogic.swift
+//  CommonLibs
+//
+//  Created by Anton Gubarenko on 14.05.2022.
+//
+
+import Foundation
