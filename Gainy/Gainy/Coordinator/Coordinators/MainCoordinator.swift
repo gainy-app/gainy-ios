12abc656bffb4b5b139fe5beaf09a6058df3a4ab
Anton Gubarenko <anton@gainy.app>
2022-07-14 08:58:24 +0300

    Analytics enhanced

--- Gainy/Gainy/Coordinator/Coordinators/MainCoordinator.swift
+++ Gainy/Gainy/Coordinator/Coordinators/MainCoordinator.swift
@@ -220,7 +220,13 @@ final class MainCoordinator: BaseCoordinator, CoordinatorFinishOutput {
         vc.modalTransitionStyle = .coverVertical
         vc.modalPresentationStyle = .fullScreen
         router.showDetailed(vc)
-        GainyAnalytics.logEvent("show_purchase_view")
+        
+        let variants = [Product.month(RemoteConfigManager.shared.monthPurchaseVariant ?? .a).identifier,
+                        Product.month6(RemoteConfigManager.shared.month6PurchaseVariant ?? .a).identifier,
+                        Product.year(RemoteConfigManager.shared.yearPurchaseVariant ?? .a).identifier
+        ]
+        
+        GainyAnalytics.logEvent("show_purchase_view", params: ["variants" : variants])
     }
     
     func showPromoPurchaseView() {
