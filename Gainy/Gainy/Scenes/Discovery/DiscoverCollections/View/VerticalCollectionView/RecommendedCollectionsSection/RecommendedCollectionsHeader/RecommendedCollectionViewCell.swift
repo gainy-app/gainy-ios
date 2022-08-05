cbb0f5b0d57c06ffb966cef34f38eec93d9f8d08
Anton Gubarenko <anton@gainy.com>
2022-08-05 16:51:48 +0300

    Discovery: add/remove icons updated

--- Gainy/Gainy/Scenes/Discovery/DiscoverCollections/View/VerticalCollectionView/RecommendedCollectionsSection/RecommendedCollectionsHeader/RecommendedCollectionViewCell.swift
+++ Gainy/Gainy/Scenes/Discovery/DiscoverCollections/View/VerticalCollectionView/RecommendedCollectionsSection/RecommendedCollectionsHeader/RecommendedCollectionViewCell.swift
@@ -56,7 +56,7 @@ final class RecommendedCollectionViewCell: RoundedCollectionViewCell {
         
         plusButton.autoPinEdge(.trailing, to: .trailing, of: contentView, withOffset: 0)
         plusButton.autoPinEdge(.bottom, to: .bottom, of: contentView, withOffset: 0)
-        plusButton.autoSetDimensions(to: .init(width: 48, height: 48))
+        plusButton.autoSetDimensions(to: .init(width: 56, height: 56))
         
         layer.isOpaque = true
         fillRemoteBack()
@@ -85,7 +85,7 @@ final class RecommendedCollectionViewCell: RoundedCollectionViewCell {
         imageView.contentMode = .scaleAspectFill
         imageView.isOpaque = true        
         imageView.backgroundColor = .lightGray
-        imageView.layer.cornerRadius = 16
+        imageView.layer.cornerRadius = 17
         imageView.clipsToBounds = true
         return imageView
     }()
