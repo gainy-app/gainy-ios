24fdab1f0958a3c91c87d2e1552208ea51f09a5e
Serhii Borysov <serhii@beat.no>
2022-08-10 14:18:25 +0100

    Change default ascending settings

--- Gainy/Gainy/Managers/CollectionsDetailsSettingsManager.swift
+++ Gainy/Gainy/Managers/CollectionsDetailsSettingsManager.swift
@@ -194,7 +194,7 @@ final class CollectionsDetailsSettingsManager {
         if let settings = settings?[id] {
             return settings
         } else {
-            let defSettigns = CollectionSettings(collectionID: id, sorting: MarketDataField.matchScore, ascending: false, viewMode: .grid, pieChartMode: .categories, pieChartSelected: false)
+            let defSettigns = CollectionSettings(collectionID: id, sorting: MarketDataField.matchScore, ascending: true, viewMode: .grid, pieChartMode: .categories, pieChartSelected: false)
             settings?[id] = defSettigns
             return defSettigns
         }
