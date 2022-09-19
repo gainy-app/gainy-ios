a595831989e5d7f9888c325bfca94789662da48a
Serhii Borysov <nahashsvb@gmail.com>
2022-09-19 10:27:04 +0100

    Add GainyButton to commons

---
+++ Frameworks/GainyCommon/GainyCommon/Views/GainyButton.swift
@@ -0,0 +1,115 @@
+//
+//  GainyButton.swift
+//  GainyCommon
+//
+//  Created by Serhii Borysov on 9/13/22.
+//
+
+import UIKit
+
+class GainyButton: UIButton {
+       
+    open var buttonActionHandler: ((UIButton) -> ())? = nil
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupView()
+    }
+    
+    public func configureWithCornerRadius(radius: Float) {
+        
+        layer.cornerRadius = CGFloat(radius)
+        layer.masksToBounds = false
+    }
+    
+    public func configureWithBorder(borderWidth: Float, borderColor: UIColor) {
+        
+        layer.borderWidth = CGFloat(borderWidth)
+        layer.borderColor = borderColor.cgColor
+    }
+    
+    public func configureWithBackgroundColor(color: UIColor) {
+        
+        self.bgColor = color
+        self.setNeedsLayout()
+        self.layoutIfNeeded()
+    }
+    
+    public func configureWithDisabledBackgroundColor(color: UIColor) {
+        
+        self.bgColorDisabled = color
+        self.setNeedsLayout()
+        self.layoutIfNeeded()
+    }
+    
+    public func configureWithHighligtedBackgroundColor(color: UIColor) {
+        
+        self.bgColorDisabled = color
+        self.setNeedsLayout()
+        self.layoutIfNeeded()
+    }
+    
+    public func configureWithTitle(title: String, color: UIColor, state: UIControl.State) {
+        
+        self.setTitleColor(color, for: state)
+        self.setTitle(title, for: state)
+    }
+    
+    public func configureWithFont(font: UIFont) {
+        
+        self.titleLabel?.font = font
+    }
+    
+    private var bgColor: UIColor = UIColor.init(hexString: "#E7EAEE") ?? UIColor.blue
+    private var bgColorDisabled: UIColor = UIColor.init(hexString: "#0062FF") ?? UIColor.lightGray
+    private var bgColorHighlighted: UIColor = UIColor.init(hexString: "#0062FF") ?? UIColor.blue.withAlphaComponent(0.5)
+    
+    override var isHighlighted: Bool {
+        
+        didSet {
+            self.backgroundColor = isHighlighted ? self.bgColorHighlighted : self.bgColor
+        }
+    }
+    
+    override var isEnabled: Bool {
+        
+        didSet {
+            self.backgroundColor = !isEnabled ? self.bgColorDisabled : self.bgColor
+        }
+    }
+    
+    private func setupView() {
+        
+        self.configureWithTitle(title: "Button Normal", color: UIColor.white, state: .normal)
+        self.configureWithTitle(title: "Button Disabled", color: UIColor.white, state: .disabled)
+        self.configureWithTitle(title: "Button Highlighted", color: UIColor.white, state: .highlighted)
+        self.configureWithFont(font: UIFont.systemFont(ofSize: 16.0, weight: .medium))
+        self.configureWithCornerRadius(radius: 16.0)
+        addTarget(self, action: #selector(touchDown), for: [.touchDown, .touchDragEnter])
+        addTarget(self, action: #selector(touchUp), for: [.touchUpInside, .touchDragExit, .touchCancel])
+        addTarget(self, action: #selector(touchUpInside), for: [.touchUpInside,])
+    }
+    
+    
+    @objc private func touchDown() {
+        
+    }
+    
+    @objc private func touchUp() {
+        self.transform = CGAffineTransform(scaleX: 0.8, y: 0.8)
+        UIView.animate(withDuration: 0.5, delay: 0.0, usingSpringWithDamping: 0.5, initialSpringVelocity: 5, options: .curveEaseInOut, animations: {
+                        self.transform = CGAffineTransform.identity
+        }, completion: nil)
+    }
+    
+    @objc private func touchUpInside() {
+        
+        self.buttonActionHandler?(self)
+    }
+}
+
