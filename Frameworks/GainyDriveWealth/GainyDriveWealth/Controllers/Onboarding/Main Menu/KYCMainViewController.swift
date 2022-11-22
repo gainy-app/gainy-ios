c8ab76e43afd7b13e248ef7d85c19e3f91bd4e0e
Anton Gubarenko <anton@gainy.app>
2022-11-22 13:25:32 +0300

    Small adjustment

--- Frameworks/GainyDriveWealth/GainyDriveWealth/Controllers/Onboarding/Main Menu/KYCMainViewController.swift
+++ Frameworks/GainyDriveWealth/GainyDriveWealth/Controllers/Onboarding/Main Menu/KYCMainViewController.swift
@@ -207,7 +207,7 @@ final class KYCMainViewController: DWBaseViewController {
                                 if sendFormSuccess {
                                     // TODO: KYC - what to do after send form?
                                     print("Successfully send KYC form")
-                                    self.coordinator?.showOrderSpaceDone(amount: 0, collectionId: 0, name: "", mode: .kyc)
+                                    self.coordinator?.showOrderSpaceDone(amount: 0, collectionId: 0, name: "", mode: .kycApproved)
                                     
                                 } else {
                                     print("Error: Failed to send KYC form!")
