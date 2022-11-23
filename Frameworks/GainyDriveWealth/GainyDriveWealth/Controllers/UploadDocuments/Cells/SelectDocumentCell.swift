495cb05d15eb55c8880419c1a14583033c52bd15
Zhenya Taran <yauheni.taran@lemellabs.com>
2022-11-23 15:22:50 +0300

    Documents: Added select document view screen

---
+++ Frameworks/GainyDriveWealth/GainyDriveWealth/Controllers/UploadDocuments/Cells/SelectDocumentCell.swift
@@ -0,0 +1,8 @@
+//
+//  SelectDocumentCell.swift
+//  GainyDriveWealth
+//
+//  Created by Евгений Таран on 23.11.22.
+//
+
+import Foundation
---
+++ Frameworks/GainyDriveWealth/GainyDriveWealth/Controllers/UploadDocuments/SelectDocumentsToUploadViewController.swift
@@ -0,0 +1,8 @@
+//
+//  SelectDocumentsToUploadViewController.swift
+//  GainyDriveWealth
+//
+//  Created by Евгений Таран on 23.11.22.
+//
+
+import Foundation
