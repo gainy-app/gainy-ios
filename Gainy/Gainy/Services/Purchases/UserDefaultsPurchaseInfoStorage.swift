59844578574d3d55055d7e1717530713037f6b45
Anton Gubarenko <anton@gainy.app>
2022-12-04 13:46:55 +0300

    TTF Unblur

--- Gainy/Gainy/Services/Purchases/UserDefaultsPurchaseInfoStorage.swift
+++ Gainy/Gainy/Services/Purchases/UserDefaultsPurchaseInfoStorage.swift
@@ -38,42 +38,42 @@ class UserDefaultsPurchaseInfoStorage: PurchaseInfoStorageProtocol {
     }
     
     func getViewedCollections() {
-        OneSignal.getTags({[weak self] tags in
-            var viewedCols: [Int] = []
-            var viewedDate: Date?
-            for tag in (tags ?? [:]) {
-                if tag.key as? String == UserDefaultsPurchaseInfoStorage.colKey {
-                    viewedCols = (tag.value as? String)?.split(separator: ",").compactMap({Int($0)}) ?? []
-                }
-                if tag.key as? String == UserDefaultsPurchaseInfoStorage.firstAddKey, let date = UserDefaultsPurchaseInfoStorage.iso8601DateFormatter.date(from: (tag.value as? String) ?? "") {
-                    viewedDate = date
-                }
-            }
-            
-            //Handling
-            if let viewedDate = viewedDate {
-                //Getting month after first view
-                let monthAfter = viewedDate + 1.months
-                
-                if Date() < monthAfter {
-                    self?.viewedCollections = viewedCols
-                } else {
-                    //Reset
-                    viewedCols = []
-                    self?.viewedCollections = viewedCols
-                    //Updating OS
-                    OneSignal.sendTag(UserDefaultsPurchaseInfoStorage.colKey, value: viewedCols.compactMap({String($0)}).joined(separator: ","))
-                    OneSignal.sendTag(UserDefaultsPurchaseInfoStorage.firstAddKey, value: UserDefaultsPurchaseInfoStorage.iso8601DateFormatter.string(from: Date()))
-                }
-            } else {
-                self?.viewedCollections = viewedCols
-            }
-            
-            self?.collectionsViewedSubject.value = self?.viewedCount ?? 0
-            self?.firstAddDate = viewedDate
-        }, onFailure: { error in
-            dprint("Error getting tags - \(error?.localizedDescription)")
-        })
+//        OneSignal.getTags({[weak self] tags in
+//            var viewedCols: [Int] = []
+//            var viewedDate: Date?
+//            for tag in (tags ?? [:]) {
+//                if tag.key as? String == UserDefaultsPurchaseInfoStorage.colKey {
+//                    viewedCols = (tag.value as? String)?.split(separator: ",").compactMap({Int($0)}) ?? []
+//                }
+//                if tag.key as? String == UserDefaultsPurchaseInfoStorage.firstAddKey, let date = UserDefaultsPurchaseInfoStorage.iso8601DateFormatter.date(from: (tag.value as? String) ?? "") {
+//                    viewedDate = date
+//                }
+//            }
+//
+//            //Handling
+//            if let viewedDate = viewedDate {
+//                //Getting month after first view
+//                let monthAfter = viewedDate + 1.months
+//
+//                if Date() < monthAfter {
+//                    self?.viewedCollections = viewedCols
+//                } else {
+//                    //Reset
+//                    viewedCols = []
+//                    self?.viewedCollections = viewedCols
+//                    //Updating OS
+//                    OneSignal.sendTag(UserDefaultsPurchaseInfoStorage.colKey, value: viewedCols.compactMap({String($0)}).joined(separator: ","))
+//                    OneSignal.sendTag(UserDefaultsPurchaseInfoStorage.firstAddKey, value: UserDefaultsPurchaseInfoStorage.iso8601DateFormatter.string(from: Date()))
+//                }
+//            } else {
+//                self?.viewedCollections = viewedCols
+//            }
+//
+//            self?.collectionsViewedSubject.value = self?.viewedCount ?? 0
+//            self?.firstAddDate = viewedDate
+//        }, onFailure: { error in
+//            dprint("Error getting tags - \(error?.localizedDescription)")
+//        })
     }
     
     private let collectionsViewedSubject: CurrentValueSubject<Int, Never> = CurrentValueSubject.init(0)
@@ -83,20 +83,21 @@ class UserDefaultsPurchaseInfoStorage: PurchaseInfoStorageProtocol {
     }
     
     func isViewedCollection(_ colId: Int) -> Bool {
-        viewedCollections.contains(colId)
+        //viewedCollections.contains(colId)
+        return true
     }
     
     func viewCollection(_ colId: Int) -> Bool {
-        guard viewedCollections.count < collectionViewLimit else {return false}
-        if !viewedCollections.contains(colId) {
-            viewedCollections.append(colId)
-            collectionsViewedSubject.value = viewedCount
-            OneSignal.sendTag(UserDefaultsPurchaseInfoStorage.colKey, value: viewedCollections.compactMap({String($0)}).joined(separator: ","))
-            if firstAddDate == nil {
-                firstAddDate = Date()
-                OneSignal.sendTag(UserDefaultsPurchaseInfoStorage.firstAddKey, value: UserDefaultsPurchaseInfoStorage.iso8601DateFormatter.string(from: Date()))
-            }
-        }
+//        guard viewedCollections.count < collectionViewLimit else {return false}
+//        if !viewedCollections.contains(colId) {
+//            viewedCollections.append(colId)
+//            collectionsViewedSubject.value = viewedCount
+//            OneSignal.sendTag(UserDefaultsPurchaseInfoStorage.colKey, value: viewedCollections.compactMap({String($0)}).joined(separator: ","))
+//            if firstAddDate == nil {
+//                firstAddDate = Date()
+//                OneSignal.sendTag(UserDefaultsPurchaseInfoStorage.firstAddKey, value: UserDefaultsPurchaseInfoStorage.iso8601DateFormatter.string(from: Date()))
+//            }
+//        }
         return true
     }
     
