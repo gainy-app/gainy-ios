301288bdc05d7175e718cb5e3446f3c4f8a7b6e0
Anton Gubarenko <anton@gainy.app>
2022-12-20 17:03:13 +0300

    Porto: percent update

--- Gainy/Gainy/Scenes/Portfolio/Plaid/Models/HoldingsModelMapper.swift
+++ Gainy/Gainy/Scenes/Portfolio/Plaid/Models/HoldingsModelMapper.swift
@@ -100,7 +100,7 @@ struct HoldingsModelMapper {
                                              todayGrow: TickerLiveStorage.shared.getSymbolData(symbol)?.priceChangeToday ?? 0.0,
                                              absoluteGains: absGains,
                                              relativeGains: relGains,
-                                             percentInProfile: (holdingGroup.details?.valueToPortfolioValue ?? 0.0) * 100.0,
+                                             percentInProfile: (holdingGroup.gains?.valueToPortfolioValue ?? 0.0) * 100.0,
                                              securities: securities,
                                              securityTypes: holdingGroup.holdings.compactMap({$0.holdingDetails?.securityType}),
                                              holdingDetails: holdingGroup.details,
