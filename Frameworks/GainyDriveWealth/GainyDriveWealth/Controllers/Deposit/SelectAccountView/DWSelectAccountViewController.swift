ec9f9caa494fdc6dbb12f9ebfbef516f74d13564
Zhenya Taran <yauheni.taran@lemellabs.com>
2022-12-26 18:44:12 +0300

    DW: removed test call of upload documents

--- Frameworks/GainyDriveWealth/GainyDriveWealth/Controllers/Deposit/SelectAccountView/DWSelectAccountViewController.swift
+++ Frameworks/GainyDriveWealth/GainyDriveWealth/Controllers/Deposit/SelectAccountView/DWSelectAccountViewController.swift
@@ -138,11 +138,10 @@ extension DWSelectAccountViewController: UITableViewDelegate, UITableViewDataSou
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        coordinator?.showAddDocuments()
-//        let item = accounts[indexPath.row]
-//        userProfile.selectedFundingAccount = item
-//        dismiss(animated: true) { [weak self] in
-//            self?.dismissHandler?()
-//        }
+        let item = accounts[indexPath.row]
+        userProfile.selectedFundingAccount = item
+        dismiss(animated: true) { [weak self] in
+            self?.dismissHandler?()
+        }
     }
 }
