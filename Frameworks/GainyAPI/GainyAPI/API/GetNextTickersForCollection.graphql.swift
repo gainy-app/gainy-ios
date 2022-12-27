65cdf7f12d3bd50eb99248249f7200b80e556f0e
Anton Gubarenko <anton@gainy.app>
2022-12-27 17:23:53 +0300

    Model update. MS for TTF in Porto.

--- Frameworks/GainyAPI/GainyAPI/API/GetNextTickersForCollection.graphql.swift
+++ Frameworks/GainyAPI/GainyAPI/API/GetNextTickersForCollection.graphql.swift
@@ -61,7 +61,7 @@ public final class GetNextTickersForCollectionQuery: GraphQLQuery {
       self.init(unsafeResultMap: ["__typename": "query_root", "collections": collections.map { (value: Collection) -> ResultMap in value.resultMap }])
     }
 
-    /// fetch data from the table: "public_221209205349.profile_collections"
+    /// fetch data from the table: "public_221227091811.profile_collections"
     public var collections: [Collection] {
       get {
         return (resultMap["collections"] as! [ResultMap]).map { (value: ResultMap) -> Collection in Collection(unsafeResultMap: value) }
