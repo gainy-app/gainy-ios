3fad11dda0326d5275947a2b3de7ca487c9b2eb1
Zhenya Taran <drakzon1@gmail.com>
2023-01-19 13:56:05 +0300

    TTF History: added show more for ttf and ticker details

---
+++ Gainy/Gainy/View/Trading/HistoryView/InnerCells/HistoryConfigurators.swift
@@ -0,0 +1,8 @@
+//
+//  HistoryConfigurators.swift
+//  Gainy
+//
+//  Created by r10 on 19.01.2023.
+//
+
+import Foundation
---
+++ Gainy/Gainy/View/Trading/HistoryView/InnerCells/ShowHistoryCell/ShowHistoryCell.swift
@@ -0,0 +1,8 @@
+//
+//  ShowHistoryCell.swift
+//  Gainy
+//
+//  Created by r10 on 19.01.2023.
+//
+
+import Foundation
