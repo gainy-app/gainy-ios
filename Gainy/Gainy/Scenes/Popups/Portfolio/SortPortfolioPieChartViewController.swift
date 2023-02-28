2697c15e434e00f67bb6fb2a5d6a039b1b3ef795
Anton Gubarenko <anton@gainy.app>
2023-02-28 15:36:58 +0300

    Porto: pie sorting restored

--- Gainy/Gainy/Scenes/Popups/Portfolio/SortPortfolioPieChartViewController.swift
+++ Gainy/Gainy/Scenes/Popups/Portfolio/SortPortfolioPieChartViewController.swift
@@ -120,7 +120,7 @@ final class SortPortfolioPieChartViewController: BaseViewController {
         guard let userID = self.profileToUse else {
             return
         }
-        guard let settings = PortfolioSettingsManager.shared.getSettingByUserID(userID) else {
+        guard let settings = PortfolioSettingsManager.pieShared.getSettingByUserID(userID) else {
             return
         }
         guard !sender.isSelected else {
