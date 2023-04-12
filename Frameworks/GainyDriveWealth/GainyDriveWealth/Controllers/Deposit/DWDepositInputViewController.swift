0db333e4a271d2fe2f632248fde06b86ddf23013
Anton Gubarenko <anton@gainy.app>
2023-04-12 17:03:57 +0300

    DW: Invest min info

--- Frameworks/GainyDriveWealth/GainyDriveWealth/Controllers/Deposit/DWDepositInputViewController.swift
+++ Frameworks/GainyDriveWealth/GainyDriveWealth/Controllers/Deposit/DWDepositInputViewController.swift
@@ -134,6 +134,7 @@ final class DWDepositInputViewController: DWBaseViewController {
                         nextBtn.configureWithTitle(title: "Enter value", color: UIColor.white, state: .disabled)
                         minInvestAmount = 0.0
                     } else {
+                        subTitleLbl.text = "Minimum required \((self.coordinator?.remoteConfig.minInvestAmount ?? 0.0).price)"
                         nextBtn.configureWithTitle(title: "Minimum required \((self.coordinator?.remoteConfig.minInvestAmount ?? 0.0).price)", color: UIColor.white, state: .disabled)
                         minInvestAmount = (self.coordinator?.remoteConfig.minInvestAmount ?? 0.0)
                         AnalyticsKeysHelper.shared.fundingAccountSource = "kyc"
